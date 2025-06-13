48290d4fdb7eee2acd5c739bd98e49650b11936c
Ana T <aterezinha.19@hotmail.com>
2025-06-13 08:15:51 -0300

    Fix db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,11 +1,20 @@
 import { Pool } from 'pg';
 
-const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: parseInt(process.env.PGPORT || '5432', 10),
-});
+const pool = new Pool(
+  process.env.NODE_ENV === 'production'
+    ? {
+        connectionString: process.env.POSTGRES_URL,
+        ssl: {
+          rejectUnauthorized: false
+        }
+      }
+    : {
+        user: process.env.PGUSER,
+        host: process.env.PGHOST,
+        database: process.env.PGDATABASE,
+        password: process.env.PGPASSWORD,
+        port: parseInt(process.env.PGPORT || '5432', 10),
+      }
+);
 
 export default pool; 
